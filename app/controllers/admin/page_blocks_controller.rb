6fbe6cf945427128043e8c1aed387f79f937df50
iamdebajyotibanerjee <ips.qsf08@gmail.com>
2026-07-28 11:12:24 +0000

    Add granular styling control to each page blocks. -
    1. Testimonial block done

--- app/controllers/admin/page_blocks_controller.rb
+++ app/controllers/admin/page_blocks_controller.rb
@@ -4,9 +4,7 @@ module Admin
     before_action :set_landing_page
 
     def create
-      # Calculate the next position number automatically
       next_position = @landing_page.page_blocks.maximum(:position).to_i + 1
-
       @page_block = @landing_page.page_blocks.build(page_block_params)
       @page_block.position = next_position
 
@@ -17,6 +15,19 @@ module Admin
         end
       else
         redirect_to edit_admin_landing_page_path(@landing_page), alert: "Could not add block."
+      end
+    end
+
+    def update
+      @page_block = @landing_page.page_blocks.find(params[:id])
+
+      if @page_block.update(page_block_params)
+        respond_to do |format|
+          format.turbo_stream
+          format.html { redirect_to edit_admin_landing_page_path(@landing_page), notice: "Block updated!" }
+        end
+      else
+        render status: :unprocessable_entity
       end
     end
 
@@ -45,7 +56,31 @@ module Admin
     end
 
     def page_block_params
-      params.require(:page_block).permit(:block_type)
+      permitted = params.require(:page_block).permit(:block_type)
+
+      if params[:page_block][:content].present?
+        # Extract raw nested parameters and normalize indexed keys into Arrays
+        raw_content = params[:page_block][:content].to_unsafe_h
+        permitted[:content] = normalize_json_content(raw_content)
+      end
+
+      permitted
+    end
+
+    # Recursively converts hashes with sequential numeric keys ("0", "1", "2") into standard Arrays
+    def normalize_json_content(value)
+      case value
+      when Hash
+        if value.keys.present? && value.keys.all? { |k| k.to_s =~ /\A\d+\z/ }
+          value.keys.sort_by(&:to_i).map { |k| normalize_json_content(value[k]) }
+        else
+          value.transform_values { |v| normalize_json_content(v) }
+        end
+      when Array
+        value.map { |v| normalize_json_content(v) }
+      else
+        value
+      end
     end
   end
 end
