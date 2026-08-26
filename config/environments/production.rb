08ab160fed2e02b5aafca5ae5e46482361d3196b
iamdebajyotibanerjee <ips.qsf08@gmail.com>
2026-08-26 14:08:31 +0530

    remove public url from storage yml and route asset requests through rails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,4 +87,7 @@ Rails.application.configure do
   #
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+  # Routes file requests through your Rails app rather than generating direct bucket URLs
+  config.active_storage.resolve_model_to_route = :rails_storage_proxy
 end
